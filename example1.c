060a499d6eabc121e945b2c1ecbe671485525d3c
Aman Agarwal <amanaamazing@gmail.com>
2014-05-07 14:02:04 -0700

    example code to demonstrate on

---
+++ example1.c
@@ -0,0 +1,402 @@
+# Repeat Command
+
+bool firstFunction(int arg1, int arg2);
+bool secondFunction(int arg1, int arg2);
+bool thirdFunction(int arg1, int arg2);
+
+# Text Objects
+
+if (a && firstFunction(i, j) && b) {
+}
+
+
+[a, b, c, d, e]
+(a, b, c, d, e)
+{a, b, c, d, e}
+
+
+int safe_errno(int err)
+{
+	if (err != -1)
+	{
+		char c = someFunction();
+		while (true)
+		{
+			if (isspace(c))
+		{
+			return 0;
+		}
+			c++;
+		}
+		return -1;
+	}
+	return err;
+}
+
+
+<?xml version="1.0" encoding="ISO-8859-1" ?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+</xs:schema>
+
+# Macros
+
+Abbreviate to shorten, abridge
+
+
+
+Abstinence the act of refraining from pleasurable activity, e.g., eating or drinking
+
+
+
+Adulation high praise
+
+
+
+Adversity misfortune, an unfavorable turn of events
+
+
+
+Aesthetic pertaining to beauty or the arts
+
+
+
+Amicable friendly, agreeable
+
+
+
+Anachronistic out-of-date, not attributed to the correct historical period
+
+
+
+Anecdote short, usually funny account of an event
+
+
+
+Anonymous nameless, without a disclosed identity
+
+
+
+Antagonist foe, opponent, adversary
+
+
+
+Arid extremely dry or deathly boring
+
+
+
+Assiduous persistent, hard-working
+
+
+
+Asylum sanctuary, shelter, place of refuge
+
+
+
+Benevolent friendly and helpful
+
+
+
+Camaraderie trust, sociability amongst friends
+
+
+
+Censure to criticize harshly
+
+
+
+Circuitous indirect, taking the longest route
+
+
+
+Clairvoyant exceptionally insightful, able to foresee the future
+
+
+
+Collaborate to cooperate, work together
+
+
+
+Compassion sympathy, helpfulness or mercy
+
+
+
+Compromise to settle a dispute by terms agreeable to both sides
+
+
+
+Condescending possessing an attitude of superiority, patronizing
+
+
+
+Conditional depending on a condition, e.g., in a contract
+
+
+
+Conformist person who complies with accepted rules and customs
+
+
+
+Congregation a crowd of people, an assembly
+
+
+
+Convergence state of separate elements joining or coming together
+
+
+
+Deleterious harmful, destructive, detrimental
+
+
+
+Demagogue leader, rabble-rouser, usually appealing to emotion or prejudice
+
+
+
+Digression the act of turning aside, straying from the main point, esp. in a speech or argument
+
+
+
+Diligent careful and hard-working
+
+
+
+Discredit harm the reputation of, dishonor or disgrace
+
+
+
+Disdain to regard with scorn or contempt
+
+
+
+Divergent separating, moving in different directions from a particular point
+
+
+
+Empathy identification with the feelings of others
+
+
+
+Emulate to imitate, follow an example
+
+
+
+Enervating weakening, tiring
+
+
+
+Enhance improve, bring to a greater level of intensity
+
+
+
+Ephemeral momentary, transient, fleeting
+
+
+
+Evanescent quickly fading, short-lived, esp. an image
+
+
+
+Exasperation irritation, frustration
+
+
+
+Exemplary outstanding, an example to others
+
+
+
+Extenuating excusing, lessening the seriousness of guilt or crime, e.g., of mitigating factors
+
+
+
+Florid red-colored, flushed; gaudy, ornate
+
+
+
+Fortuitous happening by luck, fortunate
+
+
+
+Frugal thrifty, cheap
+
+
+
+Hackneyed cliched, worn out by overuse
+
+
+
+Haughty arrogant and condescending
+
+
+
+Hedonist person who pursues pleasure as a goal
+
+
+
+Hypothesis assumption, theory requiring proof
+
+
+
+Impetuous rash, impulsive, acting without thinking
+
+
+
+Impute to attribute an action to particular person or group
+
+
+
+Incompatible opposed in nature, not able to live or work together
+
+
+
+Inconsequential unimportant, trivial
+
+
+
+Inevitable certain, unavoidable
+
+
+
+Integrity decency, honesty, wholeness
+
+
+
+Intrepid fearless, adventurous
+
+
+
+Intuitive instinctive, untaught
+
+
+
+Jubilation joy, celebration, exultation
+
+
+
+Lobbyist person who seeks to influence political events
+
+
+
+Longevity long life
+
+
+
+Mundane ordinary, commonplace
+
+
+
+Nonchalant calm, casual, seeming unexcited
+
+
+
+Novice apprentice, beginner
+
+
+
+Opulent wealthy
+
+
+
+Orator lecturer, speaker
+
+
+
+Ostentatious showy, displaying wealth
+
+
+
+Parched dried up, shriveled
+
+
+
+Perfidious faithless, disloyal, untrustworthy
+
+
+
+Precocious unusually advanced or talented at an early age
+
+
+
+Pretentious pretending to be important, intelligent or cultured
+
+
+
+Procrastinate to unnecessarily delay, postpone, put off
+
+
+
+Prosaic relating to prose; dull, commonplace
+
+
+
+Prosperity wealth or success
+
+
+
+Provocative tending to provoke a response, e.g., anger or disagreement
+
+
+
+Prudent careful, cautious
+
+
+
+Querulous complaining, irritable
+
+
+
+Rancorous bitter, hateful
+
+
+
+Reclusive preferring to live in isolation
+
+
+
+Reconciliation the act of agreement after a quarrel, the resolution of a dispute
+
+
+
+Renovation repair, making something new again
+
+
+
+Resilient quick to recover, bounce back
+
+
+
+Restrained controlled, repressed, restricted
+
+
+
+Reverence worship, profound respect
+
+
+
+Sagacity wisdom
+
+
+
+Scrutinize to observe carefully
+
+
+
+Spontaneity impulsive action, unplanned events
+
+
+
+Spurious lacking authenticity, false
+
+
+
+Submissive tending to meekness, to submit to the will of others
+
+
+
+Substantiate to verify, confirm, provide supporting evidence
+
+
+
+Subtle hard to detect or describe; perceptive
+Please allow access to your computer’s microphone to use Voice Recording.
+Having trouble? Click here for help.
+
